165a55152f7fb30f0dad1841d080799f65fa58f0
Ahmed Ali <35417149+ahmed-alii@users.noreply.github.com>
2020-05-03 03:15:35 +0500

    Protecting API KEY

--- connection/firebaseConfig.js
+++ connection/firebaseConfig.js
@@ -1,9 +1,10 @@
+// REPLACE THIS WITH YOUR OWN...
 export default {
-    apiKey: "AIzaSyBx6H1PMrZHbISbHtxMxAVZM_pY-j15BJA",
-    authDomain: "recipe-4c4a7.firebaseapp.com",
-    databaseURL: "https://recipe-4c4a7.firebaseio.com",
-    projectId: "recipe-4c4a7",
-    storageBucket: "recipe-4c4a7.appspot.com",
-    messagingSenderId: "994468678330",
-    appId: "1:994468678330:web:b2ee61378ea11462f11083"
+    apiKey: "*********************",
+    authDomain: "*********************",
+    databaseURL: "*********************",
+    projectId: "*********************",
+    storageBucket: "*********************",
+    messagingSenderId: "*********************",
+    appId: "*********************"
 };
